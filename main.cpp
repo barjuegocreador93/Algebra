f4296071ab8d596775eda146541576517e34b35a
Camilo Barbosa <barbosa93cg@gmail.com>
2016-02-06 14:10:21 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "polinomy.h"
 int main()
 {
- polinomy a, b, c;
+ polinomy a, b, c, d;
 
  a.setMonomy(1,"x,2");
  a.setMonomy(1,"z,1");
- b.setMonomy(1,"x,2");
+ b.setMonomy(-1,"x,2");
  b.setMonomy(1,"z,1");
 
  cout<<"dy/dx[ ( ";
@@ -16,6 +16,9 @@ int main()
 
  c=(a.der("x")*b)+(a*b.der("x"));
  c.print();cout<<"\n";
+ cout<<"si x=2 y z=3 = ";
+ d=c.infunc("x=2,z=3");
+ d.print();
 
 
 
